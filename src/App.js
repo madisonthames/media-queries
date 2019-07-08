8205096350da913c509ed20e1354281f27b361b4
madison <madisonpaigethames@gmail.com>
2019-07-08 13:57:07 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+import './reset.css'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: 'menu-drop-down'
+    }
+  }
+
+  handleClick = () => {
+    if
+    (this.state.menuStatus === 'active') {
+      this.setState({menuStatus: 'unactive'})
+    } else {
+      this.setState({menuStatus: 'active'})
+    }
+  }
+  
+  
+  render() {
+    return (
+      <main>
+        <div className='fullNav'>
+            <div className='title'>
+              <h2>Start Bootstrap</h2>
+            </div>
+
+            <div className='list-items'>
+                <p className='item'>SERVICES</p>
+                <p className='item'>PORTFOLIO</p>
+                <p className='item'>ABOUT</p>
+                <p className='item'>TEAM</p>
+                <p className='item'>CONTACT</p>
+            </div>
+
+              <button 
+              onClick={this.handleClick}
+              className='headerButton'>
+                MENU <i class="fas fa-bars"></i>
+              </button>
+
+              <div className={this.state.menuStatus + ' menu-drop-down'}>
+              <div className='menu-drop-down-list'>
+                <p className='item'>SERVICES</p>
+                <p className='item'>PORTFOLIO</p>
+                <p className='item'>ABOUT</p>
+                <p className='item'>TEAM</p>
+                <p className='item'>CONTACT</p>
+              </div>
+            </div>
+
+              </div>
+       
+
+
+        <div className='frontImage'>
+          <div className='mainText'>
+            <h2>Welcome To Our Studio!</h2>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button 
+            className='frontImageButton'>TELL ME MORE</button>
+          </div>
+        </div>
+
+      </main>
+
+    );
+  }
 }
 
 export default App;
+
+
